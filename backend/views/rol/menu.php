9ff52242ce9df2ec6a4b69d05cc9479319977772
Gustavo Dezi <gudezi@sofse.gob.ar>
2016-09-19 16:38:34 -0300

    roles y menues 2

---
+++ backend/views/rol/menu.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\Rol */
+
+$this->title = 'Menues Rol: ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Rols', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Menues';
+$action='menu';
+?>
+<div class="rol-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+        'listaOpciones' => $listaOpciones,
+        'action' => $action
+    ]) ?>
+
+</div>
